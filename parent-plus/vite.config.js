429c9caa8ea0721ca33d8571ca4a5c2b52f424b8
venkateshangamuthu <iamvenkateshvk@gmail.com>
2026-05-27 11:37:03 +0530

    full

--- parent-plus/vite.config.js
+++ parent-plus/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Parentplus/',
+  base: process.env.VERCEL ? '/' : '/Parentplus/',
   server: {
     proxy: {
       '/api': {
